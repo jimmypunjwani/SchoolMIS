e3b6e487eb23601d75853ed667f810ccfc1f6cb6
Jimmy Punjwani <jimmy.punjwani@itrebp.org>
2021-10-18 01:40:59 +0500

    Updated all pacakges except bootstrap 3.4.1 and Update _layout and home-index page, and update bundle config file.

--- SchoolMIS/App_Start/BundleConfig.cs
+++ SchoolMIS/App_Start/BundleConfig.cs
@@ -11,6 +11,10 @@ namespace SchoolMIS
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            //Including jquery-ui path:
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -22,9 +26,11 @@ namespace SchoolMIS
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            //Adding jquery ui folder autocomplete:
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/themes/base/autocomplete.css"));
         }
     }
 }
